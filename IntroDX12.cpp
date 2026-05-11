2b4159e50cdaa91d72b33eb3d8a24a02259525ab
Yamamoto Keita <55564997+K-MountainBook@users.noreply.github.com>
2026-05-11 10:58:41 +0900

    プロジェクト ファイルを追加します。

---
+++ IntroDX12.cpp
@@ -0,0 +1,154 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+
+// DirectX12 headers
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+
+// Static library linking
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+using namespace DirectX;
+
+// DX12系Intefaceのポインタを宣言
+ID3D12Device* _dev = nullptr;
+IDXGIFactory6* _dxgiFactory = nullptr;
+IDXGISwapChain4* _swapChain = nullptr;
+
+// D3Dの機能レベルの配列	
+D3D_FEATURE_LEVEL levels[] = {
+	D3D_FEATURE_LEVEL_12_1,
+	D3D_FEATURE_LEVEL_12_0,
+	D3D_FEATURE_LEVEL_11_1,
+	D3D_FEATURE_LEVEL_11_0,
+};
+
+XMFLOAT3 verticles[3] = {
+	{-1.0f, -1.0f, 0.0f},
+	{-1.0f, 1.0f, 0.0f},
+	{1.0f, -1.0f, 0.0f}
+}; //頂点3個
+
+// コマンドアロケータ
+ID3D12CommandAllocator* _cmdAllocator = nullptr;
+// コマンドリスト
+ID3D12GraphicsCommandList* _cmdList = nullptr;	
+// コマンドキュー
+ID3D12CommandQueue* _cmdQueue = nullptr;
+
+
+HRESULT result;
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wpram, LPARAM lparam) {
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wpram, lparam);
+}
+
+int main() {
+
+	// ウィンドウの生成
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;
+	w.lpszClassName = _T("DX12Sample");
+	w.hInstance = GetModuleHandle(nullptr);
+
+	RegisterClassEx(&w);
+
+	RECT wrc = { 0, 0, 1280, 720 };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindow(w.lpszClassName, _T("DX12Test"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, wrc.right - wrc.left, wrc.bottom - wrc.top, nullptr, nullptr, w.hInstance, nullptr);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+
+	D3D_FEATURE_LEVEL featureLevel;
+	for(auto lv:levels) {
+		if (D3D12CreateDevice(nullptr,lv,IID_PPV_ARGS(&_dev)) == S_OK){
+			featureLevel = lv;
+			break;
+		}
+	}
+
+	// アダプタが見つからなかった場合はエラーで終了
+	if (&featureLevel == nullptr) {
+		return -1;
+	}
+
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&_cmdAllocator));
+	if (result != S_OK) {
+		return -1;
+	}
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _cmdAllocator, nullptr, IID_PPV_ARGS(&_cmdList));
+	if (result != S_OK) {
+		return -1;
+	}
+
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	cmdQueueDesc.NodeMask = 0;
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+	if (result != S_OK) {
+		return -1;
+	}
+
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+	swapchainDesc.Width = 1280;
+	swapchainDesc.Height = 720;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+
+	swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue,
+		hwnd,
+		&swapchainDesc,
+		nullptr,
+		nullptr,
+		(IDXGISwapChain1**)&_swapChain);
+	if (result != S_OK) {
+		return -1;
+	}
+
+	// メインループの定義
+	{
+		MSG msg = {};
+
+
+		while (true) {
+			if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+				TranslateMessage(&msg);
+				DispatchMessageW(&msg);
+			}
+
+			if (msg.message == WM_QUIT) {
+				break;
+			}
+		}
+	}
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+}
