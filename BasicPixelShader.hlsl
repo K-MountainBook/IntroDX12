e9b5a1f298b34482d8cfc419f57c5600bf7461e0
K-MountainBook <keita.yamamoto.1009@gmail.com>
2026-05-13 18:17:53 +0900

    202605131817

--- BasicPixelShader.hlsl
+++ BasicPixelShader.hlsl
@@ -1,8 +1,8 @@
 #include "BasicShaderHeader.hlsli"
 
 
-float4 BasicPS(Output input): SV_TARGET
+float4 BasicPS(Output input) : SV_TARGET
 {
     // return float4(tex.Sample(smp,input.uv));
-    return float4(input.normal.xyz,1);
+    return float4(input.normal.xyz, 1);
 }
