04094909587346dfcfddc29f6cbce8ae76e64726
K-MountainBook <keita.yamamoto.1009@gmail.com>
2026-06-04 15:34:48 +0900

    202606041534_スフィア途中

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -5,14 +5,14 @@ float4 BasicPS(Output input) : SV_TARGET
     
     float3 light = normalize(float3(1, -1, 1));
     float brightness = dot(-light, input.normal);
+    float2 normalUV = input.normal.xy;
     
-    float4 texColor = tex.Sample(smp, input.uv);
-    
-    return float4(brightness, brightness, brightness, 1)
+    return float4(brightness, brightness, brightness, 1) // 輝度
         // テクスチャが出るようになる
         // * tex.Sample(smp, input.uv)
         * diffuse
-        * texColor
+        * tex.Sample(smp, input.uv)
+        * sph.Sample(smp, normalUV)
     ;
     //return float4(tex.Sample(smp, input.uv));
     // return float4(input.uv, 1, 1);
